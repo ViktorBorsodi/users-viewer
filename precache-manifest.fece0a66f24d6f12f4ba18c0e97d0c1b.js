41720b737cac02cc033fb62aa9c29d31d38df791
Viktor Borsodi <borsodiviktor@gmail.com>
2020-03-22 13:45:02 +0100

    Updates

---
+++ precache-manifest.fece0a66f24d6f12f4ba18c0e97d0c1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4890cfb3ff00db95d50c082734dd2f5e",
+    "url": "/users-viewer/index.html"
+  },
+  {
+    "revision": "d926ce39350338dd6746",
+    "url": "/users-viewer/static/css/main.a46f6727.chunk.css"
+  },
+  {
+    "revision": "7f767187da5118c03eba",
+    "url": "/users-viewer/static/js/2.1858398a.chunk.js"
+  },
+  {
+    "revision": "bc51ad6b075e5b96cf9ad2ad3d22ac0d",
+    "url": "/users-viewer/static/js/2.1858398a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d926ce39350338dd6746",
+    "url": "/users-viewer/static/js/main.666c7f5a.chunk.js"
+  },
+  {
+    "revision": "bc8dc836f9d7cedf8cd9",
+    "url": "/users-viewer/static/js/runtime-main.b8293106.js"
+  }
+]);
